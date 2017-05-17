cca91fef3b1759db6d696da1098731a90d7fdf17
Saundra Barrett <sbarre5@emory.edu>
2017-05-17 19:21:49 -0400

    Added welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  
+  root 'welcome#index'
   get '/project/:id', to: 'projects#show', as: 'project'
   post '/project/:id' => 'projects#create_story'
   get 'projects/index'
