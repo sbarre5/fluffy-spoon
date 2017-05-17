c674d1b70a27c9e8afc599add57f24a00ec061f8
Saundra Barrett <sbarre5@emory.edu>
2017-05-17 19:25:07 -0400

    heroku debugging

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  
+
   root 'welcome#index'
-  get '/project/:id', to: 'projects#show', as: 'project'
-  post '/project/:id' => 'projects#create_story'
-  get 'projects/index'
 
   resources :projects
 
