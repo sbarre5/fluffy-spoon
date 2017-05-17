a44168ae72d4cbc52d4ff0d59d9a3cfee9c08110
Saundra Barrett <sbarre5@emory.edu>
2017-05-17 19:59:58 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get '/project/:id', to: 'projects#show', as: 'project'
+  post '/project/:id' => 'projects#create_story'
+  get 'projects/index'
 
   resources :projects
 
